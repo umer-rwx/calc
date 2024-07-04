a54bef50729b3ee5f9ca2c117f96981b6e4048e9
umer-rwx <umertariqsocial@gmail.com>
2024-07-04 15:01:42 +0500

    First working model

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // I'm sorry for creating these abominations
 "use strict";
 
-const displayTop = document.getElementById("display-top");
+const displayTopNum = document.getElementById("number");
+const displayTopOp = document.getElementById("operator");
+
 const displayBottom = document.getElementById("display-bottom");
 
 const numberButtons = document.getElementsByClassName("number");
@@ -13,9 +15,58 @@ const clearButton = document.getElementById("ac");
 for (let x = 0; x < numberButtons.length; x++) {
     numberButtons[x].addEventListener("click", function() {
         if (displayBottom.textContent.length >= 20) return;
-
         if (displayBottom.textContent.includes(".") && numberButtons[x].textContent === ".") return;
+
         displayBottom.textContent = displayBottom.textContent + numberButtons[x].textContent;
     })
 }
 
+for (let x = 0; x < operatorButtons.length; x++) {
+    operatorButtons[x].addEventListener("click", function() {
+        if (operatorButtons[x].textContent === "=") {
+            operate();
+
+            if (displayTopNum.textContent != false) displayBottom.textContent = displayTopNum.textContent;
+
+            displayTopOp.textContent = null;
+            displayTopNum.textContent = null;
+            return;
+        }
+
+
+        if (displayTopOp.textContent != false) {
+            operate();
+        } else {
+            displayTopNum.textContent = displayBottom.textContent;
+            displayBottom.textContent = "";
+        }
+
+        displayTopOp.textContent = operatorButtons[x].textContent;
+    })
+}
+
+function operate() {
+    let result;
+    switch (displayTopOp.textContent) {
+        case "+":
+            result = Number(displayTopNum.textContent) + Number(displayBottom.textContent);
+            displayTopNum.textContent = String(result);
+            break;
+        case "-":
+            result = Number(displayTopNum.textContent) - Number(displayBottom.textContent);
+            displayTopNum.textContent = String(result);
+            break;
+        case "*":
+            result = Number(displayTopNum.textContent) * Number(displayBottom.textContent);
+            displayTopNum.textContent = String(result);
+            break;
+        case "/":
+            result = Number(displayTopNum.textContent) / Number(displayBottom.textContent);
+            displayTopNum.textContent = String(result);
+            break;
+        case "=":
+            displayBottom.textContent = displayTopNum.textContent;
+            return; // To skip the line which clears the bottom text
+    }
+    displayBottom.textContent = "";
+}
